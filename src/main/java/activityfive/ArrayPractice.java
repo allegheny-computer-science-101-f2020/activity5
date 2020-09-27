e41c5e67198e76719b10703f06b35beca4bc2868
Janyl Jumadinova <jjumadinova@allegheny.edu>
2020-09-26 20:37:38 -0400

    Starter files for activity 5

---
+++ src/main/java/activityfive/ArrayPractice.java
@@ -0,0 +1,16 @@
+package activityfive;
+
+/**
+ * This class provides a template for experimentation with 1D array.
+ *
+ * @author Janyl Jumadinova
+ */
+public class ArrayPractice {
+
+  /**
+   * Create two arrays and then practice copying and comparing them.
+   */
+  public static void main(String[] args) {
+
+  }
+}
